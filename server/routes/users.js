558204eae83d650fe4f472db4a0cf2055efdae7d
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-12 01:58:45 +0530

    Provided JWT tokens to all the routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from '../controllers/user.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
 // update user
-router.put("/:id", update)
+router.put("/:id", verifyToken, update)
 
 // delete user
-router.delete("/:id", deleteUser)
+router.delete("/:id", verifyToken, deleteUser)
 
 // get a user
 router.get("/find/:id", getUser)
 
 // subscribe user
-router.put("/sub/:id", subscribe)
+router.put("/sub/:id", verifyToken, subscribe)
 
 // unsubscribe user
-router.put("/unsub/:id", unsubscribe)
+router.put("/unsub/:id", verifyToken, unsubscribe)
 
 // like a video
-router.put("/like/:videoId", like)
+router.put("/like/:videoId", verifyToken, like)
 
 // dislike a video
-router.put("/dislike/:videoId", dislike)
+router.put("/dislike/:videoId", verifyToken, dislike)
 
 export default router;
