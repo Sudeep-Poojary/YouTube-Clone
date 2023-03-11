7db10da9af01739895e1b7842835a91906c582a7
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-12 02:31:43 +0530

    Created routes for videos

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,13 @@
 import express from 'express';
-import {  } from '../controllers/video.js';
+import { addVideo } from '../controllers/video.js';
+import verifyToken from '../verifyToken.js';
 
 const router = express.Router();
+
+// Create a Video
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", addVideo)
 
 export default router;
