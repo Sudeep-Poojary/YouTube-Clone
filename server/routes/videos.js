742b2e6c9f39d9b20c681b6d4e8b2cde468f7554
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-14 00:54:21 +0530

    Addes routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -9,5 +9,8 @@ router.post("/", verifyToken, addVideo)
 router.put("/:id", verifyToken, addVideo)
 router.delete("/:id", verifyToken, addVideo)
 router.get("/find/:id", addVideo)
+router.put("/view/:id", addVideo)
+router.put("/view/:id", addVideo)
+
 
 export default router;
