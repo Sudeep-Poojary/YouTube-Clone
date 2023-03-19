06059264c3b89632d5811466438af6d8f387aa4b
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-03-19 17:39:30 +0530

    Added more routes like views, trends, random, etc

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { addVideo } from '../controllers/video.js';
-import verifyToken from '../verifyToken.js';
+import { addVideo, addView, getByTag, random, search, sub, trend } from '../controllers/video.js';
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
@@ -9,8 +9,13 @@ router.post("/", verifyToken, addVideo)
 router.put("/:id", verifyToken, addVideo)
 router.delete("/:id", verifyToken, addVideo)
 router.get("/find/:id", addVideo)
-router.put("/view/:id", addVideo)
-router.put("/view/:id", addVideo)
+router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub", verifyToken, sub)
+router.get("/tags", getByTag)
+router.get("/search", search)
+
 
 
 export default router;
