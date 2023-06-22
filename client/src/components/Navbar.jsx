ee207a744d53c2aee9fae7a9b4333f6c4654c262
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-06-22 23:01:21 +0530

    chore: Used useSelector hook for SignIn button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   position: sticky;
@@ -53,6 +54,7 @@ const Button = styled.button`
 `;
 
 const Navbar = () => {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Container>
       <Wrapper>
@@ -61,12 +63,16 @@ const Navbar = () => {
           <SearchOutlinedIcon />
         </Search>
 
-        <Link to="signin" style={{ textDecoration: "none" }}>
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
-        </Link>
+        {currentUser ? (
+          "user"
+        ) : (
+          <Link to="signin" style={{ textDecoration: "none" }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
+        )}
       </Wrapper>
     </Container>
   );
