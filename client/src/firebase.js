533c9cd10748b225b106870d735f44d98b88135c
Sudeep-Poojary <sudeeppoojary0320@gmail.com>
2023-06-28 01:46:47 +0530

    chore: Initialized firebase and GoogleAuthProvider

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import dotenv from "dotenv";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE,
+  authDomain: "video-2ef06.firebaseapp.com",
+  projectId: "video-2ef06",
+  storageBucket: "video-2ef06.appspot.com",
+  messagingSenderId: "854887565157",
+  appId: "1:854887565157:web:c0b27c88eae77dcc0a038c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+
+export const provider = new GoogleAuthProvider();
+
+export default app;
